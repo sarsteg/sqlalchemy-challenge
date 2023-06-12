4265b673f58bb5ea9f76c59ca0bd4a03aa4d80c8
sarsteg <85451611+sarsteg@users.noreply.github.com>
2023-06-12 18:08:48 -0500

    update flask

--- app.py
+++ app.py
@@ -41,77 +41,85 @@ app = Flask(__name__)
 # Static Routes
 
 @app.route("/")
-def index():
-    """List all available api routes."""
+def home():
+    """List all available routes."""
     return (
         f"Available Routes:<br/>"
-        f"<br/>"
-        f"Precipitation: api/v1.0/precipitation<br/>"
-        f"List of stations: api/v1.0/stations<br/>"
-        f"Temperature observations: api/v1.0/tobs<br/>"
-        f"Temperature stats from start date: api/v1.0/<start><br/>"
-        f"Temperature stats from start to end date: api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    """Return the last 12 months of precipitation data as JSON."""
     session = Session(engine)
-    sel = [measurement.date,measurement.prcp]
-    query_result = session.query(*sel).all()
+    last_date = session.query(func.max(Measurement.date)).scalar()
+    one_year_ago = (pd.to_datetime(last_date) - pd.DateOffset(years=1)).strftime('%Y-%m-%d')
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= one_year_ago).all()
     session.close()
+    
+    precipitation_data = {date: prcp for date, prcp in results}
+    return jsonify(precipitation_data)
 
-    precipitation = []
-    for date, prcp in query_result:
-        prcp_dict = {}
-        prcp_dict["Date"] = date
-        prcp_dict["Precipitation"] = prcp
-        precipitation.append(prcp_dict)
-
-    return jsonify(precipitation)
-
-@app.rout("api/v1.0/stations")
+@app.route("/api/v1.0/stations")
 def stations():
+    """Return a list of stations from the dataset as JSON."""
     session = Session(engine)
-    sel = [station.station, station.name, station.latitude, station.longitude, station.elevation]
-    query_result = session.query(*sel).all()
+    results = session.query(Station.station).all()
     session.close()
-
-    stations = []
-    for station, name, lat, lon, el in query_result:
-        station_dict = {}
-        station_dict["Station"] = station
-        station_dict["Name"] = name
-        station_dict["Lat"] = lat
-        station_dict["Lon"] = lon
-        station_dict["Elevation"] = el
-        stations.append(station_dict)
-
-    return jsonify(stations)
+    
+    stations_list = list(np.ravel(results))
+    return jsonify(stations_list)
 
 @app.route("/api/v1.0/tobs")
+def tobs():
+    """Return the temperature observations of the most-active station for the previous year as JSON."""
     session = Session(engine)
-    lateststr = session.query(measurement.date).order_by(measurement.date.desc()).first()[0]
-    latestdate = dt.datetime.strptime(lateststr, '%Y-%m-%d')
-    querydate = dt.date(latestdate.year -1, latestdate.month, latestdate.day)
-    sel = [Measurement.date,Measurement.tobs]
-    queryresult = session.query(*sel).filter(Measurement.date >= querydate).all()
+    last_date = session.query(func.max(Measurement.date)).scalar()
+    one_year_ago = (pd.to_datetime(last_date) - pd.DateOffset(years=1)).strftime('%Y-%m-%d')
+    most_active_station = session.query(Measurement.station).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.id).desc()).first()
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active_station[0]).\
+        filter(Measurement.date >= one_year_ago).all()
     session.close()
-
-    tobsall = []
-    for date, tobs in queryresult:
-        tobs_dict = {}
-        tobs_dict["Date"] = date
-        tobs_dict["Tobs"] = tobs
-        tobsall.append(tobs_dict)
-
-    return jsonify(tobsall)
-
-@app.route("/api/v1.0/<start>")
-
+    
+    tobs_list = [{"date": date, "tobs": tobs} for date, tobs in results]
+    return jsonify(tobs_list)
 
 # Dynamic Routes
 
+@app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
+def temperature_stats(start, end=None):
+    """Return the temperature statistics (TMIN, TAVG, TMAX) for a specified start or start-end range as JSON."""
+    session = Session(engine)
+    
+    if end:
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+    else:
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).all()
+    
+    session.close()
+    
+    if results:
+        temperature_stats = {
+            "TMIN": results[0][0],
+            "TAVG": results[0][1],
+            "TMAX": results[0][2]
+        }
+        return jsonify(temperature_stats)
+    else:
+        return jsonify({"error": "Invalid date range"}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
