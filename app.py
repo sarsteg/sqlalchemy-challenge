b3733e256bf9e56b279ccdd671eac7772def2db8
sarsteg <85451611+sarsteg@users.noreply.github.com>
2023-06-12 13:49:39 -0500

    start of flask

--- app.py
+++ app.py
@@ -1,30 +1,57 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+
+
+@app.route("/api/v1.0/precipitation")
+
+
+@app.rout("api/v1.0/stations")
+
+
+@app.route("/api/v1.0/tobs")
+
+
+@app.route("/api/v1.0/<start>")
+
+
+@app.route("/api/v1.0/<start>/<end>")
+
